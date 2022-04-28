f7744f686c9525d626aed24b6887e37c154d8f86
binhdv <vanbinhdoan971@gmail.com>
2022-04-28 17:56:44 +0700

    'qwe'

--- tiktok-live.py
+++ tiktok-live.py
@@ -1,8 +1,15 @@
+import pandas as pd
+import atexit
+import signal
+import sys
+
 from TikTokLive import TikTokLiveClient
 from TikTokLive.types.events import CommentEvent, ConnectEvent, GiftEvent, ShareEvent, LikeEvent, FollowEvent, ViewerCountUpdateEvent
+ 
+listComment = [];
 
 client: TikTokLiveClient = TikTokLiveClient(
-    unique_id="quynhalee", **(
+    unique_id="trangdinhgiay", **(
         {
             "enable_extended_gift_info": True
         }
@@ -16,10 +23,14 @@ async def on_connect(_: ConnectEvent):
 @client.on("comment")
 async def on_connect(event: CommentEvent):
     print(f"{event.user.uniqueId} -> {event.comment}")
+    listComment.append({'username': event.user.uniqueId, 'Comment': event.comment})
+    # df = pd.DataFrame({'Username':[event.user.uniqueId],'Comment':[event.comment]})
+    # df.to_excel('tikTokLiveData.xlsx', header=None, index=True, encoding='utf-8')
 
 @client.on("gift")
 async def on_gift(event: GiftEvent):
-    print(f"{event.user.uniqueId} sent a gift: {event.gift}!")
+    print(f"{event.user.uniqueId} sent a gift")
+    # print(f"{event.user.uniqueId} sent a {event.gift.gift_id}!")
     # for giftInfo in client.available_gifts:
     #     if giftInfo["id"] == event.gift.gift_id:
     #         print(f"Name: {giftInfo['name']} Image: {giftInfo['image']['url_list'][0]} Diamond Amount: {giftInfo['diamond_count']}")
@@ -39,6 +50,19 @@ async def on_share(event: ShareEvent):
 @client.on("viewer_count_update")
 async def on_connect(event: ViewerCountUpdateEvent):
     print("Received a new vewer count:", event.viewerCount)
+
+def exit_handler():
+    print('App is ending, going to save excel:' + listComment)
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C! Going to save excel: ')
+    df = pd.DataFrame(listComment)
+    df.to_excel('tikTokLiveData.xlsx', header=None, index=True, encoding='utf-8')
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
 
 if __name__ == '__main__':
     # Run the client and block the main thread
